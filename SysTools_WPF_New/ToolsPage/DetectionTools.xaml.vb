940b11e01277786e5554fc507ea16095bf451a73
Forever <25360809+Forever331@users.noreply.github.com>
2022-03-09 23:26:59 +0800

    Update 3/9

--- SysTools_WPF_New/ToolsPage/DetectionTools.xaml.vb
+++ SysTools_WPF_New/ToolsPage/DetectionTools.xaml.vb
@@ -1,5 +1,23 @@
 ﻿Imports ModernWpf.Media.Animation
 
 Class DetectionTools
+    Private Sub Aida64_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles Aida64.MouseDoubleClick
 
+    End Sub
+
+    Private Sub CPU_Z_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles CPU_Z.MouseDoubleClick
+
+    End Sub
+
+    Private Sub GPU_Z_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles GPU_Z.MouseDoubleClick
+
+    End Sub
+
+    Private Sub HWinfo_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles HWinfo.MouseDoubleClick
+
+    End Sub
+
+    Private Sub HWmonitor_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles HWmonitor.MouseDoubleClick
+
+    End Sub
 End Class
--- SysTools_WPF_New/ToolsPage/DiskTools.xaml.vb
+++ SysTools_WPF_New/ToolsPage/DiskTools.xaml.vb
@@ -1,3 +1,42 @@
 ﻿Class DiskTools
+    Private Sub AS_SSD_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles AS_SSD.MouseDoubleClick
+        Process.Start(".\Software Package\AS SSD Benchmark\AS SSD Benchmark.exe")
+    End Sub
+
+    Private Sub CrystalDiskInfo_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles CrystalDiskInfo.MouseDoubleClick
+
+    End Sub
+
+    Private Sub CrystalDiskMark_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles CrystalDiskMark.MouseDoubleClick
+
+    End Sub
+
+    Private Sub Disk_Benchmark_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles Disk_Benchmark.MouseDoubleClick
+
+    End Sub
+
+    Private Sub DiskGenius_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles DiskGenius.MouseDoubleClick
+
+    End Sub
+
+    Private Sub HD_Tune_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles HD_Tune.MouseDoubleClick
+
+    End Sub
+
+    Private Sub LLFTOOL_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles LLFTOOL.MouseDoubleClick
+
+    End Sub
+
+    Private Sub PartAssist_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles PartAssist.MouseDoubleClick
+
+    End Sub
+
+    Private Sub SSD_Z_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles SSD_Z.MouseDoubleClick
+
+    End Sub
+
+    Private Sub Victoria_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles Victoria.MouseDoubleClick
+
+    End Sub
 
 End Class
