2837946ecfcbc15f9e7e212a97c74fa62ca2a81a
Forever331 <25360809+Forever331@users.noreply.github.com>
2022-07-23 01:38:29 +0800

    添加检测获取的项目

--- SysTools_WPF_New/Page/Test.xaml.vb
+++ SysTools_WPF_New/Page/Test.xaml.vb
@@ -59,19 +59,19 @@ Class Test
 
     Private Sub Test()
         '操作系统信息
-        Dim OpSystem As New ManagementObjectSearcher("select * from win32_OperatingSystem")
+        Dim OpSystem As New ManagementObjectSearcher("SELECT * FROM win32_OperatingSystem")
         Dim OpS As ManagementObjectCollection = OpSystem.Get()
         '计算机信息
-        Dim cmsystem As New ManagementObjectSearcher("select * from win32_computersystem")
+        Dim cmsystem As New ManagementObjectSearcher("SELECT * FROM win32_computersystem")
         Dim cms As ManagementObjectCollection = cmsystem.Get()
         'CPU信息
-        Dim Process As New ManagementObjectSearcher("select * from win32_Processor")
+        Dim Process As New ManagementObjectSearcher("SELECT * FROM win32_Processor")
         Dim CPUPr As ManagementObjectCollection = Process.Get()
         '网卡信息
-        Dim NetAdapter As New ManagementObjectSearcher("SELECT *FROM   Win32_NetworkAdapter WHERE Manufacturer != 'Microsoft' AND NOT PNPDeviceID LIKE 'ROOT\\%'")
+        Dim NetAdapter As New ManagementObjectSearcher("SELECT * FROM   Win32_NetworkAdapter WHERE Manufacturer != 'Microsoft' AND NOT PNPDeviceID LIKE 'ROOT\\%'")
         Dim NetAdapterDevice As ManagementObjectCollection = NetAdapter.Get()
         '局域网IP有关
-        Dim NETconfig As New ManagementObjectSearcher("select * from win32_NetworkAdapterConfiguration")
+        Dim NETconfig As New ManagementObjectSearcher("SELECT * FROM win32_NetworkAdapterConfiguration WHERE IPEnabled = True")
         Dim MNetconfig As ManagementObjectCollection = NETconfig.Get()
         '声卡信息
         Dim Sound As New ManagementObjectSearcher("SELECT * FROM Win32_SoundDevice")
@@ -79,8 +79,11 @@ Class Test
         '显卡信息
         Dim Video As New ManagementObjectSearcher("SELECT * FROM Win32_VideoController")
         Dim VideoDevice As ManagementObjectCollection = Video.Get()
+        '显示器信息
+        Dim Monitor As New ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE service='monitor'")
+        Dim MonitorService As ManagementObjectCollection = Monitor.Get()
         '屏幕分辨率
-        Dim DeskTop As New ManagementObjectSearcher("select * from Win32_VideoController")
+        Dim DeskTop As New ManagementObjectSearcher("SELECT * FROM Win32_VideoController")
         Dim DeskTopCont As ManagementObjectCollection = DeskTop.Get()
 
         Dim XmlSettings As XmlWriterSettings = New XmlWriterSettings()
@@ -138,15 +141,14 @@ Class Test
                 Xml_Writer.WriteEndElement()
 
 
-                Xml_Writer.WriteStartElement("Attribute", "局域网IP")
-                For Each MNF As ManagementObject In MNetconfig
-                    If MNF("IPEnabled") Then
-
-                        Xml_Writer.WriteElementString("Properties", "内网IP地址:" + Chr(32) + MNF.GetPropertyValue("IpAddress")(0).ToString())
-                        Xml_Writer.WriteElementString("Properties", "MAC地址:" + Chr(32) + MNF.GetPropertyValue("MACAddress").ToString())
-
-                        Exit For
-                    End If
+                Xml_Writer.WriteStartElement("Attribute", "本机IP/MAC信息")
+                For Each MNF As Object In MNetconfig
+                    Xml_Writer.WriteElementString("Properties", "网卡名称:" + Chr(32) + MNF.GetPropertyValue("Description").ToString())
+                    Xml_Writer.WriteElementString("Properties", "MAC地址:" + Chr(32) + MNF.GetPropertyValue("MACAddress").ToString())
+                    Xml_Writer.WriteElementString("Properties", "本机IPv4/6地址:")
+                    For i = 0 To UBound(MNF.GetPropertyValue("IpAddress"))
+                        Xml_Writer.WriteElementString("Properties", Chr(9) + MNF.GetPropertyValue("IPAddress")(i))
+                    Next
                 Next
                 Xml_Writer.WriteEndElement()
 
@@ -157,10 +159,22 @@ Class Test
                 Next
                 Xml_Writer.WriteEndElement()
 
+                Xml_Writer.WriteStartElement("Attribute", "显示器信息 (笔记本可获得内置屏幕型号 一般为MONITOR\后的英文+数字结构)")
+                For Each Monitor_Object As ManagementObject In MonitorService
+                    Xml_Writer.WriteElementString("Properties", "显示器名称: " + Monitor_Object.GetPropertyValue("Name").ToString())
+                    Xml_Writer.WriteElementString("Properties", "显示屏硬件ID: " + Monitor_Object.GetPropertyValue("HardwareID")(0).ToString())
 
-                Xml_Writer.WriteStartElement("Attribute", "分辨率与颜色深度(有空位表示双卡或扩展未使用)")
-                For Each DeskTop_Info As ManagementObject In DeskTopCont
-                    Xml_Writer.WriteElementString("Properties", DeskTop_Info.GetPropertyValue("VideoModeDescription"))
+                Next
+                Xml_Writer.WriteEndElement()
+
+
+                Xml_Writer.WriteStartElement("Attribute", "屏幕分辨率.颜色深度.当前刷新率(有空位表示双卡或扩展未使用)")
+                For Each DeskTop_Info As Object In DeskTopCont
+                    Try
+                        Xml_Writer.WriteElementString("Properties", DeskTop_Info.GetPropertyValue("Name") + ": " + DeskTop_Info.GetPropertyValue("VideoModeDescription").ToString() + Chr(32) + DeskTop_Info.GetPropertyValue("CurrentRefreshRate").ToString + "Hz")
+                    Catch e As NullReferenceException
+                        Xml_Writer.WriteElementString("Properties", DeskTop_Info.GetPropertyValue("Name") + ": " + "屏幕未接入")
+                    End Try
                 Next
                 Xml_Writer.WriteEndElement()
 
