ef3504387ffbca30bc76aee133f39a4bd326ef7f
Forever331 <25360809+Forever331@users.noreply.github.com>
2022-08-20 22:03:54 +0800

    修改Page载入方法便于缓存的数据

--- SysTools_WPF_New/MainWindow.xaml.vb
+++ SysTools_WPF_New/MainWindow.xaml.vb
@@ -1,6 +1,18 @@
 ﻿Imports System.IO
-Imports System.Windows.Controls
 Class MainWindow
+    Private Home_Page As New Frame With {.Content = New Home}
+    Private FastDetection As New Frame With {.Content = New Test}
+    Private DetectionTools As New Frame With {.Content = New DetectionTools}
+    Private TestTools As New Frame With {.Content = New TestTools}
+    Private DiskTools As New Frame With {.Content = New DiskTools}
+    Private PeripheralsTools As New Frame With {.Content = New PeripheralsTools}
+    Private RepairingTools As New Frame With {.Content = New RepairingTools}
+    Private AdbTools As New Frame With {.Content = New AdbTools}
+    Private ChangeWindows As New Frame With {.Content = New ChangeWindows}
+    Private WindowsActivation As New Frame With {.Content = New Activation}
+    Private AboutPage As New Frame With {.Content = New About}
+
+
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
         File.Delete("Info.xml")
         '程序启动数量限制
@@ -13,61 +25,65 @@ Class MainWindow
         AppName = Nothing
         ProcessTotal = Nothing
         Title = "SysTools Ver" + (Application.ResourceAssembly.GetName().Version.ToString())
+
+        '设置默认启动Page页
+        FrameContent.Content = Home_Page
     End Sub
 
     Private Sub Home_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Home.MouseDown
         '主页
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/Home.xaml", UriKind.Absolute))
+        FrameContent.Content = Home_Page
     End Sub
 
     Private Sub Fast_Detection_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Fast_Detection.MouseDown
         '系统配置快速检测 ( 基于 Windows Management Instrumentation )
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/Test.xaml", UriKind.Absolute))
+        FrameContent.Content = FastDetection
     End Sub
 
     Private Sub Detection_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Detection_Tools.MouseDown
         '配置检测工具 ( 仅64位系统 )
-        FrameContent.Navigate(New Uri("pack://application:,,,/ToolsPage/DetectionTools.xaml", UriKind.Absolute))
+        FrameContent.Content = DetectionTools
     End Sub
     Private Sub Test_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Test_Tools.MouseDown
         '硬件测试工具
-        FrameContent.Navigate(New Uri("pack://application:,,,/ToolsPage/TestTools.xaml", UriKind.Absolute))
+        FrameContent.Content = TestTools
     End Sub
 
     Private Sub Disk_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Disk_Tools.MouseDown
         '硬盘工具
-        FrameContent.Navigate(New Uri("pack://application:,,,/ToolsPage/DiskTools.xaml", UriKind.Absolute))
+        FrameContent.Content = DiskTools
     End Sub
 
     Private Sub Peripherals_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Peripherals_Tools.MouseDown
         '外设工具
-        FrameContent.Navigate(New Uri("pack://application:,,,/ToolsPage/PeripheralsTools.xaml", UriKind.Absolute))
+        FrameContent.Content = PeripheralsTools
     End Sub
 
     Private Sub Repairing_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Repairing_Tools.MouseDown
         '运行库安装工具
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/RepairingTools.xaml", UriKind.Absolute))
+        FrameContent.Content = RepairingTools
     End Sub
     Private Sub Adb_Tools_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Adb_Tools.MouseDown
         'Adb工具Box
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/AdbTools.xaml", UriKind.Absolute))
+        FrameContent.Content = AdbTools
         Dim Dialog As New AdbDialog()
         Dialog.ShowAsync()
     End Sub
+
+    Private Sub ChangeWindows_Page_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles ChangeWindows_Page.MouseDown
+        'Windows时间线
+        FrameContent.Content = ChangeWindows
+    End Sub
     Private Sub Windows_Activation_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Windows_Activation.MouseDown
         '系统激活工具
-        FrameContent.Navigate(New Uri("pack://application:,,,/ToolsPage/Activation.xaml", UriKind.Absolute))
+        FrameContent.Content = WindowsActivation
     End Sub
 
     Private Sub About_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles About.MouseDown
         '关于
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/About.xaml", UriKind.Absolute))
+        FrameContent.Content = AboutPage
     End Sub
 
-    Private Sub GithubRecommend_Page_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles ChangeWindows_Page.MouseDown
-        'Windows时间线
-        FrameContent.Navigate(New Uri("pack://application:,,,/Page/ChangeWindows.xaml", UriKind.Absolute))
-    End Sub
 
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         Dim AppPath As String = Directory.GetCurrentDirectory()
@@ -122,5 +138,6 @@ Class MainWindow
         sw.WriteLine("记得重启软件 我没加热更新(因为我不会 （；´д｀）ゞ)")
         sw.Close()
     End Sub
+
 
 End Class
