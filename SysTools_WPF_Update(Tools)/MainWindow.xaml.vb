6c60a1fb0148829c33361370871a3f6a011030ff
Forever331 <25360809+Forever331@users.noreply.github.com>
2022-11-29 22:20:54 +0800

    常规NuGet包更新

--- SysTools_WPF_Update(Tools)/MainWindow.xaml.vb
+++ SysTools_WPF_Update(Tools)/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
 ﻿Imports System.Net.Http
-Imports System.Xml
 Imports AutoUpdaterDotNET
 
 Class MainWindow
@@ -24,7 +23,7 @@ Class MainWindow
 
     Sub AutoUpdateVersion()
         AutoUpdater.HttpUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
-        AutoUpdater.UpdateFormSize = New System.Drawing.Size(800, 600)
+        AutoUpdater.UpdateFormSize = New System.Drawing.Size(700, 500)
         AutoUpdater.Start("https://systools.hksstudio.work/Tools_Update/Tools_AutoUpdate.xml")
         Version_Verify.Text = "已获取更新 若不小心关闭请关闭该窗口重新启动"
     End Sub
