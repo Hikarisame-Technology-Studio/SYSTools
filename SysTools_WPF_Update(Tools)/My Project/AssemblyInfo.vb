3064040d6a5e3e23440a0712579b726045eca1ca
Forever331 <25360809+Forever331@users.noreply.github.com>
2022-11-29 22:42:16 +0800

    降低版本号 以便新用户下载更新包

--- SysTools_WPF_Update(Tools)/My Project/AssemblyInfo.vb
+++ SysTools_WPF_Update(Tools)/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 '通过使用 "*"，如下所示:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.0.0.5")>
-<Assembly: AssemblyFileVersion("0.0.0.5")>
+<Assembly: AssemblyVersion("0.0.0.1")>
+<Assembly: AssemblyFileVersion("0.0.0.1")>
